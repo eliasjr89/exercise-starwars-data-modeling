159e586b8b4b7659154a1774930361e6412e2ff8
Elías JR <167987056+eliasjr89@users.noreply.github.com>
2024-09-09 19:51:01 +0000

    one favorites

--- src/models.py
+++ src/models.py
@@ -13,73 +13,48 @@ class User(Base):
     name =  Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     
-    character_fav = relationship("Character_fav", back_populates="user")
-    planet_fav = relationship("Planet_fav", back_populates="user")
-    starship_fav = relationship("Starship_fav", back_populates="user")
+    favorites = relationship("Favorite", back_populates="user")
 
-
-class Character(Base):
-    __tablename__ = "Character"
+class Favorite(Base):
+    __tablename__ = "favorites"
     id = Column(Integer, primary_key=True)
-    name =  Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey("User.id"), nullable=False)
+    favorite_type = Column(String(50), nullable=False)
+    favorite_id = Column(Integer, nullable=False)
+    
+    user = relationship("User", back_populates="favorites")
+    favorite = relationship("FavoriteItem", back_populates="favorite")
+
+class FavoriteItem(Base):
+    __tablename__ = "favorite_items"
+    id = Column(Integer, primary_key=True)
+    type = Column(String(50), nullable=False)
+    name = Column(String(250), nullable=False)
+    attribute1 = Column(String(100))
+    attribute2 = Column(String(100))
+
+    favorite = relationship("Favorite", back_populates="favorite")
+
+class Character(FavoriteItem):
+    __tablename__ = "characters"
+    id = Column(Integer, ForeignKey("favorite_items.id"), primary_key=True)
     height = Column(Float, nullable=False)
     mass = Column(Float, nullable=False)
-    hair_color = Column(String(250), nullable=False)
-    skin_color = Column(String(250), nullable=False)
+    hair_color = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable=False)
 
-    character_fav = relationship("Character_fav", back_populates="character")
-
-
-class Character_fav(Base):
-    __tablename__ = "Character_fav"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("User.id"))
-    character_id = Column(Integer, ForeignKey("Character.id"))
-
-    user = relationship("User", back_populates="character_fav")
-    character = relationship("Character", back_populates="character_fav")
-
-
-class Planet(Base):
-    __tablename__ = "Planet"
-    id = Column(Integer, primary_key=True)
-    name =  Column(String(250), nullable=False)
+class Planet(FavoriteItem):
+    __tablename__ = "planets"
+    id = Column(Integer, ForeignKey("favorite_items.id"), primary_key=True)
     population = Column(Integer, nullable=False)
-    terrain = Column(String(250), nullable=False)
-    climate = Column(String(250), nullable=False)   
+    terrain = Column(String(50), nullable=False)
+    climate = Column(String(50), nullable=False)
 
-    planet_fav = relationship("Planet_fav", back_populates="planet")
-
-
-class Planet_fav(Base):
-    __tablename__ = "Planet_fav"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("User.id"))
-    planet_id = Column(Integer, ForeignKey("Planet.id"))
-
-    user = relationship("User", back_populates="planet_fav")
-    planet = relationship("Planet", back_populates="planet_fav")
-
-
-class Starship(Base):
-    __tablename__ = "Starship"
-    id = Column(Integer, primary_key=True)
-    name =  Column(String(250), nullable=False)
-    model = Column(String(250), nullable=False)
-    manufacturer = Column(String(250), nullable=False)
-    cargo_capacity = Column(Integer, nullable=False)     
-
-    starship_fav = relationship("Starship_fav", back_populates="starship")
-
-
-class Starship_fav(Base):
-    __tablename__ = "Starship_fav"
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey("User.id"))
-    starship_id = Column(Integer, ForeignKey("Starship.id"))
-
-    user = relationship("User", back_populates="starship_fav")
-    starship = relationship("Starship", back_populates="starship_fav")
-
+class Starship(FavoriteItem):
+    __tablename__ = "starships"
+    id = Column(Integer, ForeignKey("favorite_items.id"), primary_key=True)
+    model = Column(String(50), nullable=False)
+    manufacturer = Column(String(50), nullable=False)
+    cargo_capacity = Column(Integer, nullable=False)
 
 render_er(Base, "diagram.png")
